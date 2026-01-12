0374201124cf3c7ae53f8a9c7698804edcf62a5e
cloudstrife5661-afk <cloudstrife.5661@gmail.com>
2026-01-12 18:39:51 -0300

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,1 +1,23 @@
+const url = "https://raw.githubusercontent.com/SEU_USUARIO/galeria-anime/main/personagens.json";
 
+fetch(url)
+.then(res => res.json())
+.then(data => {
+  const lista = document.getElementById("lista");
+
+  data.personagens.forEach(p => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <img src="${p.capa}">
+      <span>${p.nome}</span>
+    `;
+
+    card.onclick = () => {
+      window.location.href = `galeria.html?id=${p.id}`;
+    };
+
+    lista.appendChild(card);
+  });
+});
